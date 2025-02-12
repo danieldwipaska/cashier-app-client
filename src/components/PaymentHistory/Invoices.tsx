53bceec55fd2044c56cb0eac40950d771f0ba583
Daniel Kamasi <danielkamasi@outlook.com>
2025-02-12 21:38:01 +0700

    Fix invoice features

--- src/components/PaymentHistory/Invoices.tsx
+++ src/components/PaymentHistory/Invoices.tsx
@@ -1,5 +1,7 @@
 import React, { useRef } from 'react';
 import jsPDF from 'jspdf';
+import { GoDownload } from 'react-icons/go';
+import bahariLogo from '../../assets/img/bahari-logo.webp';
 
 const Invoices = ({ selectedPaymentData, totalPaymentSelectedData }: any) => {
   const useReff = useRef<HTMLDivElement>(null);
@@ -21,15 +23,26 @@ const Invoices = ({ selectedPaymentData, totalPaymentSelectedData }: any) => {
     <div>
       <div className="hidden">
         <div ref={useReff} style={{ width: '14mm', fontSize: '3px' }}>
-          <div id="parent-modal-title" className="text-center  font-bold" style={{ fontSize: '4px' }}>
-            Receipt
+          <div id="parent-modal-title" className="flex flex-col items-center w-full" style={{ fontSize: '3px' }}>
+            <img src={bahariLogo} alt="bahari" className=" w-[20px]" style={{ marginBottom: '-5px' }} />
+            <div className="font-bold">Bahari Irish Pub</div>
+            <div>Jl. Kawi No.8A, Kota Malang</div>
+            <div>Indonesia, 65119</div>
           </div>
-          <div className="text-center border-b">Bahari Irish Pub</div>
-          <div className=" mb-1">
+          <div
+            className="w-full mt-1"
+            style={{
+              height: '1px',
+              backgroundColor: 'black',
+              transform: 'scaleY(0.2)',
+              transformOrigin: '0 0',
+            }}
+          ></div>
+          <div className="mb-1">
             <div className="flex justify-between">
               <div>Date, Time</div>
               <div>
-                {selectedPaymentData ? new Date(selectedPaymentData.created_at).toLocaleDateString() : null}, {selectedPaymentData ? new Date(selectedPaymentData.created_at).toLocaleTimeString() : null}
+                {selectedPaymentData ? selectedPaymentData.dateCreatedAt : null}, {selectedPaymentData ? selectedPaymentData.timeCreatedAt : null}
               </div>
             </div>
             <div className="flex justify-between items-center">
@@ -38,7 +51,7 @@ const Invoices = ({ selectedPaymentData, totalPaymentSelectedData }: any) => {
             </div>
             <div className="flex justify-between">
               <div>Collected</div>
-              <div>{selectedPaymentData ? selectedPaymentData.collected_by : null}</div>
+              <div>{selectedPaymentData ? selectedPaymentData.served_by : null}</div>
             </div>
             <div className="flex justify-between">
               <div>Customer</div>
@@ -59,7 +72,7 @@ const Invoices = ({ selectedPaymentData, totalPaymentSelectedData }: any) => {
             {selectedPaymentData?.type !== 'pay' ? (
               <div className="flex justify-between">
                 <div>{selectedPaymentData?.type}</div>
-                <div>IDR {Intl.NumberFormat('en-us').format(selectedPaymentData?.total_payment)}</div>
+                <div>IDR {Intl.NumberFormat('en-us').format(selectedPaymentData?.total_payment_after_tax_service)}</div>
               </div>
             ) : null}
           </div>
@@ -106,7 +119,9 @@ const Invoices = ({ selectedPaymentData, totalPaymentSelectedData }: any) => {
         </div>
       </div>
 
-      <button onClick={generatePDF}>Generate PDF</button>
+      <button onClick={generatePDF}>
+        <GoDownload size={30} />
+      </button>
     </div>
   );
 };
