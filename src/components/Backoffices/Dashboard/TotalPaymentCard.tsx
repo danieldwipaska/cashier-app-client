fef2bc1360d90c113f753c3652c2efbdfec79d4f
Daniel <danielkamasi@outlook.com>
2025-08-18 05:53:08 +0700

    Fix total payment

--- src/components/Backoffices/Dashboard/TotalPaymentCard.tsx
+++ src/components/Backoffices/Dashboard/TotalPaymentCard.tsx
@@ -16,7 +16,7 @@ const TotalPaymentCard = () => {
       const { from, to } = getOperationalHours();
 
       try {
-        const res = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/reports?from=${from}&to=${to}&status=${ReportStatus.PAID}&type=${ReportType.PAY}`, {
+        const res = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/reports?from=${from}&to=${to}&status=${ReportStatus.PAID}&type=${ReportType.PAY}&pagination=false`, {
           headers: {
             Authorization: `Bearer ${localStorage.getItem('access-token')}`,
           },
